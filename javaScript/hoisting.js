22d41b4134350ad6c0c18d9bd8a5fdf81a638a75
kajolSingh123 <69797443+kajolSingh123@users.noreply.github.com>
2024-02-10 10:02:28 +0000

    hosting

--- javaScript/hoisting.js
+++ javaScript/hoisting.js
@@ -1,21 +1,26 @@
 //+++++++++++++++++++++++++++++++Variables and Function+++++++++++++++++++++++++++++++++++++++++++++++//
-getName3();
 getName();
+// getName2 ();
+// getName3();
 console.log(x);
 console.log(getName);
 console.log(getName2);
 
 var x = 10;
 
-function getName() {          // here it behave like a function
+function getName() {
+  // here it behave like a function
   console.log("Namaste_JavaScript");
 }
-var getName3 = function (){ // it beheve like variable
 
-}
-var getName2 = () => {        // hear also behave like variable
-  console.log(`hello kajol`); 
+var getName3 = function () {
+  // it beheve like variable
+  console.log(`hello kajol`);
 };
 
+var getName2 = () => {
+  // hear also behave like variable
+  console.log(`hello kajol`);
+};
 
-// when we use arrow function it beheave another variable, in memory execution phase it reserved as variable  hoisting is 
+// when we use arrow function it beheave another variable, in memory execution phase it reserved as variable  hoisting is
