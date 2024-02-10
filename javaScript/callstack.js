26eb607e3c0debc7f30bcdf230dee1020602b556
kajolSingh123 <69797443+kajolSingh123@users.noreply.github.com>
2024-02-10 08:18:03 +0000

    hoisting

---
+++ javaScript/callstack.js
@@ -0,0 +1,12 @@
+var n = 2;
+function square(num) {
+  var ans = num * num;
+  return ans;
+}
+var square1 = square(n);
+var square4 = square(4);
+
+console.log(n);
+console.log(square1);
+
+console.log(square4);
---
+++ javaScript/hoisting.js
@@ -0,0 +1,21 @@
+//+++++++++++++++++++++++++++++++Variables and Function+++++++++++++++++++++++++++++++++++++++++++++++//
+getName3();
+getName();
+console.log(x);
+console.log(getName);
+console.log(getName2);
+
+var x = 10;
+
+function getName() {
+  console.log("Namaste_JavaScript");
+}
+var getName3 = function (){ // it beheve like variable
+
+}
+var getName2 = () => {        // hear also behave like variable
+  console.log(`hello kajol`); 
+};
+
+
+// when we use arrow function it beheave another variable, in memory execution phase it reserved as variable  hoisting is 
